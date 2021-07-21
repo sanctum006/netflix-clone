36d5daf1a090685d527bebda6645d4c2639cfacc
sanctum006 <74344331+sanctum006@users.noreply.github.com>
2021-07-21 19:10:06 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,6 @@
 import firebase from "firebase";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyAggr2w8fSoqAochnrq7NwmndN8nr1uq4I",
-  authDomain: "netflix-clone-40d26.firebaseapp.com",
-  projectId: "netflix-clone-40d26",
-  storageBucket: "netflix-clone-40d26.appspot.com",
-  messagingSenderId: "423073408608",
-  appId: "1:423073408608:web:56a3705f7e01e9ce354505",
-};
+const firebaseConfig = {};
 
 // console.log(firebase);
 
